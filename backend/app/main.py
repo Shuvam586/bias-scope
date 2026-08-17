3009772ef4dfec710c03c2e9d84c79fc49dc7725
Shuvam586 <shuvammitra05@gmail.com>
2026-08-17 20:35:13 +0000

    added more sources

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,12 +2,14 @@ from backend.app.ingestion.rss import fetch_feed
 from backend.app.ingestion.sources import SOURCES
 from backend.app.db.supabase_client import insert_articles
 
-for source in SOURCES.values():
-    print(source["name"])
-    print()
+# for source in SOURCES.values():
+#     print(source["name"])
+#     print()
 
-    for f in source["feeds"]:
-        print(f)
-        insert_articles(fetch_feed(f, source["name"]))
+#     for f in source["feeds"]:
+#         print(f)
+#         insert_articles(fetch_feed(f, source["name"]))
 
-    print()
+#     print()
+
+insert_articles(fetch_feed("https://theprint.in/feed", "The Print"))
